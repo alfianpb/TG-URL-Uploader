a5e50586a6faf41ad5c9beed2157f6fcc8bef702
alfianpb <76173717+alfianpb@users.noreply.github.com>
2021-01-09 15:29:52 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """Hello,
+    START_TEXT = """Hai Guys,
 This is a Telegram URL Upload Bot!
 
 <b>Please send me any direct download URL Link, i can upload to telegram as File/Video</b>
